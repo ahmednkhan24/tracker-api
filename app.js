317ca1e7304c91b05949d0347c37e6136212bbb6
ahmednkhan24 <ahmednkhan24@gmail.com>
2020-01-19 19:35:27 -0600

    use arrow function and send json back

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-    res.send('hello world');
+app.get('/', (req, res) => {
+    res.json({ username: 'hello world' });
+    
 });
 
 const PORT = process.env.PORT || 3000;
