80bf115b867d52c41d548f677a85dde9b6062be7
ahmednkhan24 <ahmednkhan24@gmail.com>
2020-02-09 14:40:29 -0600

    changed file names, added constants, find user by query param

--- user-service/app.js
+++ user-service/app.js
@@ -3,7 +3,7 @@ var express    = require('express'),
     cors       = require('cors'),
     sanitizer  = require('express-sanitizer'),
     bodyParser = require('body-parser'),
-    routes     = require('./routes/index');
+    routes     = require('./routes');
 
 // init
 var app = express();
