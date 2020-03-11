401f5feefd2030be6b5ccedbdce035c579418cfb
ahmednkhan24 <ahmednkhan24@gmail.com>
2020-03-11 18:18:24 -0500

    updated user to modern JS

--- src/user.js
+++ src/user.js
@@ -1,23 +1,23 @@
-var mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 // schema setup
-var userSchema = new mongoose.Schema({
-    firstName:  {
-        type:String,
-        required: true,
-    },
-    lastName: {
-        type:String,
-        required: true,
-    },
-    email: {
-        type:String,
-        required: true,
-        unique: true
-    }
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
 });
 
 // compile the schema into a model
-var userModel = mongoose.model('User', userSchema);
+const userModel = mongoose.model('User', userSchema);
 
 module.exports = userModel;
