b7c2b83d1f17acb338b5e35ffd5ccb0f95e815a2
ahmednkhan24 <ahmednkhan24@gmail.com>
2020-03-11 18:16:45 -0500

    updated service to modern JS

--- src/service.js
+++ src/service.js
@@ -1,29 +1,29 @@
-var User  = require("./user"),
-    utils = require('./utils');
+import User from './user';
+// import utils from './utils';
 
 const errorMessage = { error: 'there was an error' };
 
 const findAllUsers = async () => {
-    const users = await User.find({}, (err, allUsers) => {
-        if (err) {
-            return errorMessage;
-        }
-        return { allUsers: allUsers };
-    });
-    return users;
+  const users = await User.find({}, (err, allUsers) => {
+    if (err) {
+      return errorMessage;
+    }
+    return { allUsers };
+  });
+  return users;
 };
 
-const findUser = async emailAddress => {
-    const user = await User.find({ 'email': emailAddress }, (err, data) => {
-        if (err) {
-            return errorMessage;
-        }
-        return { foundUser: data };
-    });
-    return user;
+const findUser = async (emailAddress) => {
+  const user = await User.find({ email: emailAddress }, (err, data) => {
+    if (err) {
+      return errorMessage;
+    }
+    return { foundUser: data };
+  });
+  return user;
 };
 
 module.exports = {
-    findAllUsers,
-    findUser
-}
+  findAllUsers,
+  findUser,
+};
