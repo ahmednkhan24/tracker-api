28c1ce2821feb749abf9125f5ffb229a6ded9f39
ahmednkhan24 <ahmednkhan24@gmail.com>
2020-03-16 12:13:46 -0500

    updated unit tests

--- __tests__/utils/index.test.js
+++ __tests__/utils/index.test.js
@@ -4,4 +4,26 @@ describe('isOjectEmpty', () => {
   it('should return true if given an empty object', () => {
     expect(isObjectEmpty({})).toBe(true);
   });
+
+  it('should return false if given a non-empty object', () => {
+    expect(isObjectEmpty({ hello: 'world' })).toBe(false);
+  });
+});
+
+describe('MissingKeyInObject', () => {
+  const obj = {
+    key1: 'value1',
+    key2: 'value2',
+    key3: 'value3',
+  };
+
+  it('should return false if all keys are in the given object', () => {
+    const keys = ['key1', 'key2', 'key3'];
+    expect(missingKeyInObject(obj, keys)).toBe(false);
+  });
+
+  it('should return true if a key is missing in the given object', () => {
+    const keys = ['key1', 'key2', 'key3', 'key4'];
+    expect(missingKeyInObject(obj, keys)).toBe(true);
+  });
 });
