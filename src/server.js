185a02e917467f5719aa9925edc294265eb57233
ahmednkhan24 <ahmednkhan24@gmail.com>
2020-04-16 23:42:33 -0500

    removed host

--- src/server.js
+++ src/server.js
@@ -1,8 +1,7 @@
 export default (api) => {
   const PORT = process.env.PORT || 3000;
-  const HOST = process.env.HOST || '127.0.0.1';
 
-  const server = api.listen(PORT, HOST, () => {
+  const server = api.listen(PORT, () => {
     console.log('API Server has started');
     console.log(`Server running in ${process.env.NODE_ENV} mode`);
     const { address, port } = server.address();
