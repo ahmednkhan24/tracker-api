037614dceb562e0268198f5897946328d21a10fa
ahmednkhan24 <ahmednkhan24@gmail.com>
2020-04-21 14:17:04 -0500

    moved swagger files to src folder

--- src/api.js
+++ src/api.js
@@ -1,9 +1,6 @@
 import express from 'express';
 import sanitizer from 'express-sanitizer';
 import bodyParser from 'body-parser';
-import swaggerUi from 'swagger-ui-express';
-import swaggerLocalDoc from '../swaggerLocal.json';
-import swaggerWebDoc from '../swaggerWeb.json';
 import routes from './routes';
 
 // init
@@ -19,12 +16,7 @@ api.use(sanitizer());
 // remove etag's in order to manually configure HTTP response codes
 api.disable('etag');
 
-// swagger for API visualization/interaction
-if (process.env.NODE_ENV !== 'production') {
-  api.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerLocalDoc));
-} else {
-  api.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerWebDoc));
-}
+api.use(express.static('files'));
 
 // use our defined routes
 api.use(routes);
