cf4ce1503ef4f77ac0a479a583a1c7a19fa187fd
ahmednkhan24 <ahmednkhan24@gmail.com>
2020-06-16 12:18:01 -0500

    test root route

--- __tests__/controllers/index.test.js
+++ __tests__/controllers/index.test.js
@@ -1,15 +1,9 @@
 // eslint-disable-next-line no-unused-vars
 import polyfill from 'babel-polyfill';
 import supertest from 'supertest';
-import app from '../src/api';
+import app from '../../src/api';
 
 const request = supertest(app);
-
-describe('Sample Test', () => {
-  it('should test that true === true', () => {
-    expect(true).toBe(true);
-  });
-});
 
 describe('GET endpoints', () => {
   it('should GET root route', async (done) => {
