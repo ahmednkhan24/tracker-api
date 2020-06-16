10b95945ef7997b4fc6e1969c5df751ff5a9cc4b
ahmednkhan24 <ahmednkhan24@gmail.com>
2020-06-16 12:19:29 -0500

    starting user tests

---
+++ __tests__/controllers/user.test.js
@@ -0,0 +1,15 @@
+// // eslint-disable-next-line no-unused-vars
+// import polyfill from 'babel-polyfill';
+// import supertest from 'supertest';
+// import app from '../../src/api';
+
+// const request = supertest(app);
+
+// describe('GET endpoints', () => {
+//   it('should GET root route', async (done) => {
+//     const response = await request.get('/');
+//     expect(response.status).toBe(200);
+//     expect(response.body.hello).toBe('world');
+//     done();
+//   });
+// });
